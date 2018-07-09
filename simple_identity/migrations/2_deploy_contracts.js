6daa3b74b59a0260848254ec74970865e27fce3d
Simon Tan Yu Jing <simon.tan.yu.jing@gmail.com>
2018-07-10 00:16:28 +0800

    adding gitignore

--- simple_identity/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-//var SimpleBank = artifacts.require("./SimpleBank.sol");
-var SimpleIdentity = artifacts.require("./SimpleIdentity.sol");
-
-module.exports = function(deployer) {
-  //deployer.deploy(SimpleBank);
-  deployer.deploy(SimpleIdentity);
-};
