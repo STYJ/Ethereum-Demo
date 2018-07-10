3fb6942784f2449398869b7965eb9b7ea763464d
Simon Tan Yu Jing <simon.tan.yu.jing@gmail.com>
2018-07-10 18:01:50 +0800

    updated uint to int to handle overflow

--- simple_identity/contracts/SimpleIdentity.sol
+++ simple_identity/contracts/SimpleIdentity.sol
@@ -3,30 +3,28 @@ pragma solidity ^0.4.10;
 contract SimpleIdentity{
 
 	mapping(address => Identity) private identities;
-	uint public numIdentities;
+	int public numIdentities;
 	address public contractOwner;
 
 	// Struct for Identity
 	struct Identity {
 		string name;
-		uint age;
+		int age;
 	}
 
 	// Events
-	event LogNewIdentity(string _name, uint _age, uint numIdentities);
-	event LogDeletedIdentity(string _deletedName, uint _deletedAge, uint numIdentities);
-	event LogUpdatedIdentityName(string _updatedName, uint _age, uint numIdentities);
-	event LogUpdatedIdentityAge(string _name, uint _updatedAge, uint numIdentities);
+	event LogNewIdentity(string _name, int _age, int numIdentities);
+	event LogDeletedIdentity(string _deletedName, int _deletedAge, int numIdentities);
+	event LogUpdatedIdentityName(string _updatedName, int _age, int numIdentities);
+	event LogUpdatedIdentityAge(string _name, int _updatedAge, int numIdentities);
 
 	// Modifiers
 	// Checks that age is > 0
-	// This check is useless, you should check on the web3 end.
-	// The reason being if you pass a negative number into uint, it will be converted into a very large positive uint
-	// The require statement will never fail. 
-	// For example, if you did positiveAge(-4) and within some other function, you added 10 to the age, your age will be 6. 
-	// This is a very clear example of integer overflow. Without the +10 (or any number bigger than the modulus of your negative number)
-	// you will return an extremely large positive uint.
-	modifier positiveAge(uint _age)
+	// This check is useless if your parameter is of type uint because when a negative value is casted to a uint, you get a very large int number
+	// This require statement can never fail. 
+	// The solution to this is to use int instead of uint. 
+	// By default, int is int256
+	modifier positiveAge(int _age)
 	{
 		require(_age > 0);
 		_;
@@ -52,11 +50,11 @@ contract SimpleIdentity{
 	}
 
 	// Add identity
-	function addIdentity(string _name, uint _age) 
+	function addIdentity(string _name, int _age) 
 		public 
 		noPreviousIdentity() 
-		// positiveAge(_age)
-		returns (uint)
+		positiveAge(_age)
+		returns (int)
 	{
 		numIdentities += 1;
 		emit LogNewIdentity(_name, _age, numIdentities);
@@ -68,7 +66,7 @@ contract SimpleIdentity{
 	function removeIdentity()
 		public
 		identityExist()
-		returns (uint)
+		returns (int)
 	{
 		numIdentities -= 1;
 		emit LogDeletedIdentity(identities[msg.sender].name, identities[msg.sender].age, numIdentities);
@@ -80,7 +78,7 @@ contract SimpleIdentity{
 	function updateName(string _name)
 		public
 		identityExist()
-		returns (string, uint)
+		returns (string, int)
 	{
 		emit LogUpdatedIdentityName(_name, identities[msg.sender].age, numIdentities);
 		identities[msg.sender].name = _name;
@@ -89,11 +87,11 @@ contract SimpleIdentity{
 	}
 
 	// Update age
-	function updateAge(uint _age)
+	function updateAge(int _age)
 		public
 		identityExist()
-		// positiveAge(_age)
-		returns (string, uint)
+		positiveAge(_age)
+		returns (string, int)
 	{
 		emit LogUpdatedIdentityAge(identities[msg.sender].name, _age, numIdentities);
 		identities[msg.sender].age = _age;
@@ -105,7 +103,7 @@ contract SimpleIdentity{
 		public
 		view
 		identityExist()
-		returns (string, uint)
+		returns (string, int)
 	{
 		return(identities[msg.sender].name, identities[msg.sender].age);
 	}
