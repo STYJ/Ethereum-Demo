99997e16f39b753df490f68e44fe98f29aa89ff7
Simon Tan Yu Jing <simon.tan.yu.jing@gmail.com>
2018-09-19 11:28:42 +0800

    updated inheritance notes

--- Notes/Inheritance.sol
+++ Notes/Inheritance.sol
@@ -1,37 +1,73 @@
 pragma solidity ^0.4.24;
 
-contract C {
-  uint public u = 0; // try changing this to private and internal. If you don't realise the issue with public or internal variables, other contracts can simply inherit your contract and manipulate your public and internal variables by implementing their own setter methods.
-  function f() public {
-    u = 1;
-  }
-  
-  function getU() public constant returns (uint) {
-      return u;
-  }
+contract A {
+    
+    function hi() public pure returns (string) {
+        return "hi A";
+    }
 }
 
-contract B is C {
-  function f() public {
-    u = 2;
-  }
+contract B is A {
+    // A's hi is overridden.
+    function hi() public pure returns (string) {
+        return "hi B";
+    }
 }
 
-contract A is B {
-  function f() public {  // will set u to 3
-    u = 3;
-  }
-  // The super keyword in Solidity gives access to the immediate parent contract from which the current contract is derived.
-  function f1() public { // will set u to 2 (immediate parent and not the parent of your parent, note you can't do super.super.f() lol)
-    super.f();
-  }
-  // Alternatively, one can explicitly specifying the parent of which one wants to call the overridden function because multiple overriding steps are possible as exemplified in the example below:
-  function f2() public { // will set u to 2
-    B.f();
-  }
-  function f3() public { // will set u to 1
-    C.f();
-  }
+contract C is A {
+    // Use super to call hi from A.
+    function hi() public pure returns (string) {
+        return super.hi();
+    }
+}
+
+contract D is B, C{
+    // Super looks at C
+    // Super in C looks at A and B
+    // Since B is also A, returns hi B
+    function hi() public pure returns (string) {
+        return super.hi();
+    }
+}
+
+contract E is C, B{
+    // Super looks at B
+    // returns hi B
+    function hi() public pure returns (string) {
+        return super.hi();
+    }
+}
+
+contract F {
+    function hi() public pure returns (string) {
+        return "hi F";
+    }
+}
+
+contract G is F {
+    function hi() public pure returns (string) {
+        return super.hi();
+    }
+}
+
+
+contract H is F, C{
+    // Super looks at C
+    // Super in C looks at A and F
+    // Since different (and F is also not A), returns Hi A
+    // You can add "is A" to F and this will return Hi F instead.
+    function hi() public pure returns (string) {
+        return super.hi();
+    }
+}
+
+contract I is F, G {
+    // Super looks at G
+    // Super in G looks at F and F
+    // since same, returns F.
+    function hi() public pure returns (string) {
+        return super.hi();
+    }
 }
 
 // Note that order of importance is important. See https://ethereumdev.io/inheritance-in-solidity/
